9daa2fd120013cdf2de38e5d181a78e79cdfd4d1
Vasanth <vasanth@bizverify.org>
2022-10-26 16:22:47 +0530

    updated code

--- Source/CoreModule/Extension/UICollectionViewCell+Extension.swift
+++
@@ -1,8 +0,0 @@
-//
-//  UICollectionViewCell+Extension.swift
-//  uGlow
-//
-//  Created by sandhya on 31/08/21.
-//
-
-import UIKit
--- Source/CoreModule/Helper/Loader.swift
+++
@@ -1,8 +0,0 @@
-//
-//  Loader.swift
-//  Manipal
-//
-//  Created by DB-MM-034 on 06/10/22.
-//
-
-import UIKit
